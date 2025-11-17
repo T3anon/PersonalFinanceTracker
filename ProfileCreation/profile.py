5571e536fa7db6dd190211adceac6da4ec95d24b
Erin Whiting <erinwhiting06@gmail.com>
2025-11-16 18:28:39 -0600

    voted for webpage

--- ProfileCreation/profile.py
+++ ProfileCreation/profile.py
@@ -6,10 +6,3 @@ def create_user_profile(username,email,password,first_name,last_name):
     password = input("Enter a password:")
     first_name = input("Enter your first name:")
     last_name = input("Enter your last name:")
-
-#create user dictionary
-    user_dict = {
-        "username" : username, "email" : email, 
-        "password" : password, "first_name" : first_name, 
-        "last_name" : last_name
-        }
