e6b29e73731f30fa757cea88f408a3d39e929deb
Erin Whiting <erinwhiting06@gmail.com>
2025-11-17 19:51:20 -0600

    created user profile

--- ProfileCreation/profile.py
+++
@@ -1,8 +0,0 @@
-#Create a user profile 
-
-def create_user_profile(username,email,password,first_name,last_name):
-    username = input("Enter a username:")
-    email = input("Enter your email:")
-    password = input("Enter a password:")
-    first_name = input("Enter your first name:")
-    last_name = input("Enter your last name:")
