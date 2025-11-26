1ac38a8b1562695178b92a0610688fd5f6932f9c
T3anon <mark.swearingen2008@gmail.com>
2025-11-26 11:34:21 -0500

    Starting new register and login

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import Nav from "@/components/nav";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { User } from "./user";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
   return (
     <div>
-    <Nav items={[{href:"/",label:"Home"},{href:"/budget",label:"Budget"},{href:"/profile",label:"Profile"},{href:"/login",label:"Login"}]} />
-    </div>           
-    
+      <Nav items={[{href:"/",label:"Home"},{href:"/budget",label:"Budget"},{href:"/profile",label:"Profile"},{href:"/login",label:"Login"}]} />
+      <main>
+        <h2>Server Session</h2>
+        <pre>{JSON.stringify(session)}</pre>
+        <h2>Client Call</h2>
+        <User />
+      </main>
+    </div> 
+             
   );
 }
