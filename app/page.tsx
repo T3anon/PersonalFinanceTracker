7669bea974c50ff269e9f9b21b46d86bf3be7da2
T3anon <mark.swearingen2008@gmail.com>
2025-11-26 10:23:09 -0500

    Changed main page - update later, test nav func

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,9 @@ import Nav from "@/components/nav";
 
 export default function Home() {
   return (
-    <body>
-        <Nav items={[{href:"/",label:"Home"},{href:"/profile",label:"Profile"},{href:"/login",label:"Login"}]} />
-        
-            <div className="nav-logo">
-                <p>LOGO .</p>
-            </div>
-            <div className="nav-menu">
-                <ul>
-                    <li><a href="#" className="link">Home</a></li>
-                    <li><a href="#" className="link">Blog</a></li>
-                    <li><a href="#" className="link">Services</a></li>
-                    <li><a href="#" className="link">About</a></li>
-                </ul>
-            </div>
-            <div className="nav-button">
-                <button className="btn" id="loginBtn">Sign In</button>
-                <button className="btn" id="registerBtn">Sign Up</button>
-            </div>
-            <div className="menu-btn">
-                
-            </div>
-    </body>
+    <div>
+    <Nav items={[{href:"/",label:"Home"},{href:"/profile",label:"Profile"},{href:"/login",label:"Login"}]} />
+    </div>           
+    
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -6,8 +6,8 @@ export default function Nav({ items }:{ items:{href:string;label:string}[] }){
   return (
     <header className="nav">
       <nav className="container flex items-center justify-between h-16">
-        <Link href="/" prefetch className="font-bold">Personal Finance Tracker</Link>
-        <div className="flex gap-4">
+        {/* <Link href="/" prefetch className="font-bold">Personal Finance Tracker</Link> */}
+        <div className="flex gap-217 border-4 border-pink-500">
           {items.map(it => (
             <Link key={it.href} href={it.href} prefetch className={`hover:underline ${pathname===it.href ? "font-semibold" : ""}`}>
               {it.label}
