b549c813adb3a8e819a52dcfd0f0e02d8c5b7c8f
T3anon <mark.swearingen2008@gmail.com>
2025-12-09 20:00:15 -0500

    fixed problems that caused it to not deploy

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Suspense } from 'react'
 import { Form as LoginForm } from './form'
 
 export default function LoginPage() {
@@ -6,7 +7,9 @@ export default function LoginPage() {
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
       <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl">Login</h1>
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
         <p className="text-center">
           Need to create an account?{' '}
           <Link className="text-indigo-500 hover:underline" href="/register">
